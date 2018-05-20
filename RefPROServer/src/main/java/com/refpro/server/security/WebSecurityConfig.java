04c7b701080f406292c485e7ed88e1339a5a865d
dneshev99 <d.neshev99@gmail.com>
2018-05-20 17:59:31 +0300

    Added cors support for /login

--- RefPROServer/src/main/java/com/refpro/server/security/WebSecurityConfig.java
+++ RefPROServer/src/main/java/com/refpro/server/security/WebSecurityConfig.java
@@ -14,9 +14,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
+@EnableWebMvc
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled=true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -31,8 +41,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
 
     http.authorizeRequests().antMatchers(HttpMethod.POST,"/user/register").permitAll();
-
-    http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, "/login").permitAll().and()
+   // http.cors().and().authorizeRequests().antMatchers(HttpMethod.OPTIONS,"/**").permitAll();
+    http.csrf().disable().cors().and().authorizeRequests().antMatchers(HttpMethod.POST, "/login/").permitAll().and()
         .addFilterBefore(new JWTLoginFilter("/login", this.authenticationManager()),
             UsernamePasswordAuthenticationFilter.class)
         .addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
@@ -57,8 +67,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Autowired
   public void configAuthBuilder(AuthenticationManagerBuilder builder) throws Exception {
-
     builder.authenticationProvider(this.authProvider());
   }
+
+
+//
+//  @Bean
+//  public WebMvcConfigurer corsConfigurer() {
+//      WebMvcConfigurer webMvcConfigurer = new WebMvcConfigurerAdapter() {};
+//      CorsRegistry corsReg = new CorsRegistry();
+//      corsReg.addMapping("/**").allowedOrigins("http://localhost:4200").allowCredentials(true).allowedMethods("POST","PUT","GET","OPTIONS");
+//      webMvcConfigurer.addCorsMappings(corsReg);
+//    return webMvcConfigurer;
+//  }
+//
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfig = new CorsConfiguration().applyPermitDefaultValues();
+        corsConfig.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        source.registerCorsConfiguration("/**",corsConfig );
+        return source;
+    }
 
 }
